7d11698bb9169312b40aa0d40e8a10901f2a84be
Harrison Mfula <harrison.mfula@nokia.com>
2017-05-05 00:55:04 +0300

    Initial commit

--- src/main/scala/one/network/SimpleApp.scala
+++ src/main/scala/one/network/SimpleApp.scala
@@ -10,13 +10,13 @@ import org.apache.spark.SparkConf
 object SimpleApp extends App {
   // create Spark context with Spark configuration
   val sc = new SparkContext(new SparkConf().setAppName("Simple Spark App").setMaster("local[*]"))
-  val rdd = sc.parallelize(Seq(1,2,3,4,5))
-//  val rdd = sc.textFile("src/main/resources/test-data.txt")
-   //val res = rdd.flatMap( w => w.split(" ")).map(w=>(w,1)).reduceByKey(_+_)
-// val res = rdd.flatMap( w => w.split(" ")).map(w=>(w,1))
- // res.foreach(println)
+ // val rdd = sc.parallelize(Seq(1,2,3,4,5))
+ val rdd = sc.textFile("src/main/resources/test-data.txt")
+  val res = rdd.flatMap( w => w.split(" ")).map(w=>(w,1)).reduceByKey(_+_)
+//val res = rdd.flatMap( w => w.split(" ")).map(w=>(w,1))
+  res.foreach(println)
   //res.saveAsTextFile("pekka_food.txt")
-  //println("The most popular food for Pekka "+ res.map(_.swap).max()._2)
-  println("Harrison Running the FAT JAR "+ rdd.count())
+  println("The most popular food for Harrison is: "+ res.map(_.swap).max()._2)
+  println("Running UBER JAR  SUCCESSFUL: NUMBER OF OBJECTS IN  TEST FILE "+ rdd.count())
 
 }
